73f48db7e191da83ec1639f17dbafc6ae821e255
Brandon Foster <Brandon.Foster4525@outlook.com>
2025-03-23 15:30:41 -0400

    add modules to include path

--- msvc.bat
+++ msvc.bat
@@ -7,7 +7,7 @@ cmake --build %~dp0build/Prerequisite
 cd %~dp0build/Prerequisite/Downloads/skia
 git pull
 python3 tools/git-sync-deps
-.\bin\gn.exe gen "%~dp0build/Compile/skia" --args="extra_cflags_cc=[\"/MD\"] is_official_build=true is_component_build=true is_debug=false skia_use_system_libjpeg_turbo=false skia_use_system_zlib=false skia_use_system_expat=false skia_use_system_libwebp=false skia_use_system_libpng=false skia_use_system_harfbuzz=false skia_use_system_icu=false skia_use_icu=true"
+.\bin\gn.exe gen "%~dp0build/Compile/skia" --args="extra_cflags_cc=[\"/MD\"] is_official_build=true is_component_build=true is_debug=false skia_use_svg=true skia_enable_skshaper=true skia_use_system_libjpeg_turbo=false skia_use_system_zlib=false skia_use_system_expat=false skia_use_system_libwebp=false skia_use_system_libpng=false skia_use_system_harfbuzz=false skia_use_system_icu=false skia_use_icu=true"
 %~dp0build\Prerequisite\Downloads\ninja-%NINJA_VERSION%\ninja.exe -C "%~dp0build/Compile/skia"
 
 cmake -S %~dp0Package -B %~dp0build/Package
